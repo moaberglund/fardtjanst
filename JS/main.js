be57ae30afae47ab66121f8a6a4666cecb228511
Moa <moa.21@hotmail.com>
2024-01-03 17:40:09 +0100

    mobil-meny inlagd

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,29 @@
 "use strict";
+
+//MENYN
+
+//element
+let openBtn = document.getElementById("open-menu");
+let closeBtn = document.getElementById("close-menu");
+
+//lyssnare
+openBtn.addEventListener("click", toggleMenu);
+closeBtn.addEventListener("click", toggleMenu);
+
+//funktion
+function toggleMenu() {
+    //hitta menyn
+    let navMenuEl = document.getElementById("nav-menu");
+
+    //kolla vad display är inställt som (none/block)
+    let style = window.getComputedStyle(navMenuEl);
+
+    //ändra mellan none/block beroende på
+    if(style.display === "none") {
+        navMenuEl.style.display = "block";
+    } else {
+        navMenuEl.style.display = "none";
+    }
+
+
+}
